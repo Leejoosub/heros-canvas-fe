fe00c990ea259254ca5fb50336f51b38afcd978e
Joosub <joosublee@hotmail.com>
2024-07-02 17:58:22 -0400

    added colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,13 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+
+    colors: {
+      primary: "#0C1B33",
+      secondary: "#7A306C",
+      accentColor: "#03B5AA",
+      text: "#E6E6FA",
+    },
   },
   plugins: [],
 };
