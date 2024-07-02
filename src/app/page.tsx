7a04ba9c73973786d02aa627b78efc20fb50f7c6
Joosub <joosublee@hotmail.com>
2024-07-02 18:05:16 -0400

    temporary home page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Layout from "@/components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <div>Home Page</div>
+      <div className="flex min-w-4/5 flex-col items-center">
+        Samples and artwork will be placed below once we have some!
+      </div>
     </Layout>
   );
 }
